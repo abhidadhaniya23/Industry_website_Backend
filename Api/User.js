d7668e1a9f4cf44e1b0d121b74eabb33edde3365
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-03-06 23:24:25 +0530

    limit added with query params

--- Api/User.js
+++ Api/User.js
@@ -9,26 +9,31 @@ const user = express.Router();
 
 // admin view to show data from database
 user.get("/getData", async (req, res) => {
+  let limit = req.query.limit;
+  if (req.query.limit === undefined) {
+    limit = 500;
+  }
+  // limit = req.query.limit;
   switch (req.query.sort) {
     case "nameA":
-      const ascendingName = await User.find().sort({ name: 1 });
+      const ascendingName = await User.find().sort({ name: 1 }).limit(limit);
       res.status(200).json(ascendingName);
       break;
     case "nameD":
-      const descendingName = await User.find().sort({ name: -1 });
+      const descendingName = await User.find().sort({ name: -1 }).limit(limit);
       res.status(200).json(descendingName);
       break;
     case "message":
-      const message = await User.find().sort({ message: 1 });
+      const message = await User.find().sort({ message: 1 }).limit(limit);
       res.status(200).json(message);
       break;
     case "ascending":
-      const ascending = await User.find().sort({ created: 1 });
+      const ascending = await User.find().sort({ created: 1 }).limit(limit);
       res.status(200).json(ascending);
       break;
     default:
       // Ascending Order
-      const userData = await User.find().sort({ created: -1 });
+      const userData = await User.find().sort({ created: -1 }).limit(limit);
       if (userData.length === 0) {
         res.status(404).json({ Message: "No users are there." });
       } else {
@@ -39,10 +44,10 @@ user.get("/getData", async (req, res) => {
 });
 
 // limited responses
-user.get("/getData/:limit", async (req, res) => {
+/* user.get("/getData/:limit", async (req, res) => {
   const limitedUsers = await User.find().limit(req.params.limit).sort({ created: -1 });
   res.status(200).json(limitedUsers);
-});
+}); */
 
 // Sorting in ascending order
 
