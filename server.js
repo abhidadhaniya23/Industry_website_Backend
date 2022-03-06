6b1afdb0b20a326b6a1fb4c047eb5b4036902024
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-03-06 11:04:32 +0530

    test for env variables

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const user = require("./Api/User");
 const email = require("./Api/Email");
+require("dotenv").config();
 const PORT = 5000 || process.env.PORT;
 
 // middleware to get parsed json data from request
@@ -15,5 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
+  console.log(process.env.MONGO_URI_USERNAME);
+  console.log(process.env.MONGO_URI_PASSWORD);
   console.log(`Your app is running at ${PORT}`);
 });
