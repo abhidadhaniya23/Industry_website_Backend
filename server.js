26ae26a7f8713a4e0bafa34481b74c9b8e77a60d
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-03-06 11:34:57 +0530

    improve secutiry by adding apikey middleware

--- server.js
+++ server.js
@@ -2,17 +2,29 @@ const express = require("express");
 const app = express();
 const user = require("./Api/User");
 const email = require("./Api/Email");
+const { check } = require("express-validator");
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+const checkApi = () => {
+  return (req, res, next) => {
+    if (req.query.ApiKey === process.env.API_KEY) {
+      next();
+    } else {
+      res.status(401).json({ msg: "Unauthorized" });
+    }
+  };
+};
 
 // middleware to get parsed json data from request
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", checkApi());
 app.use("/api/user", user);
 app.use("/api/subscribe", email);
 
 app.get("/", (req, res) => {
-  res.send("Done");
+  res.status(200).send("Done");
 });
 
 app.listen(PORT, () => {
